1642182acbea5078c2c2ec2051c785b34ab94886
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2026-01-28 19:50:24 +0100

    Esercizio completato

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import com.networknt.schema.SpecVersion;
 import com.networknt.schema.ValidationMessage;
 import org.example.modelli.Persona;
 import org.example.modelli.Persone;
+import org.example.modelli.Telefono;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.*;
 import java.util.Set;
 
 public class Main {
@@ -50,6 +52,62 @@ public class Main {
                 System.out.println(p.toString());
 
 
+            String url = "jdbc:mysql://127.0.0.1:3306/";
+            String database="db_Json_Persone";
+            String user="root";
+            String password="";
+
+            Connection con;
+            con = DriverManager.getConnection(url+database,user,password);
+            System.out.println("Connesso al DB");
+
+            //INSERT persona
+            String queryPersona = "INSERT INTO persona(nome, cognome, eta) VALUES (?, ?, ?)";
+            PreparedStatement stmtPersone = con.prepareStatement(queryPersona, Statement.RETURN_GENERATED_KEYS);    //Chiede l'ID che è stato generato con l'insert
+
+            //INSERT indirizzo
+            String queryIndirizzo = "INSERT INTO indirizzo(persona_id, via, citta, cap, nazione) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement stmtIndirizzo = con.prepareStatement(queryIndirizzo);
+
+            //INSERT telefono
+            String queryTelefono = "INSERT INTO telefono(persona_id, tipo, numero) VALUES (?, ?, ?)";
+            PreparedStatement stmtTelefono = con.prepareStatement(queryTelefono);
+
+            int righeInserite = 0;
+
+            for (Persona p : persone.persone) {
+                //---- PERSONA ----
+                stmtPersone.setString(1, p.nome);
+                stmtPersone.setString(2, p.cognome);
+                stmtPersone.setInt(3, p.eta);
+                stmtPersone.executeUpdate();
+
+                //recupero ID autogenerato di persona
+                ResultSet rs = stmtPersone.getGeneratedKeys();
+                rs.next();
+                int personaId = rs.getInt(1);
+
+                //---- INDIRIZZO ----
+                stmtIndirizzo.setInt(1, personaId);
+                stmtIndirizzo.setString(2, p.indirizzo.via);
+                stmtIndirizzo.setString(3, p.indirizzo.citta);
+                stmtIndirizzo.setInt(4, p.indirizzo.cap);
+                stmtIndirizzo.setString(5, p.indirizzo.nazione);
+                stmtIndirizzo.executeUpdate();
+
+                //---- TELEFONI ----
+                for (Telefono t : p.telefoni) {
+                    stmtTelefono.setInt(1, personaId);
+                    stmtTelefono.setString(2, t.type);
+                    stmtTelefono.setString(3, t.number);
+                    stmtTelefono.executeUpdate();
+                }
+
+                righeInserite++;
+            }
+
+            con.close();
+            System.out.println("Inserite correttamente " + righeInserite + " persone");
         } catch (java.io.FileNotFoundException e) {
             System.err.println("ERRORE, file non trovati");
         } catch (Exception e) {
